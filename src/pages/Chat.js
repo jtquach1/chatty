8e4df814fd4a04e0a839cc5ce0d779642d56d090
Joyce Quach <33106214+jtquach1@users.noreply.github.com>
2020-06-02 14:06:00 -0400

    Create Chat.js

---
+++ src/pages/Chat.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { auth, db } from '../services/firebase';
+
+export default class Chat extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: auth().currentUser,
+            chats: [],
+            content: '',
+            readError: null,
+            writeError: null
+        };
+    }
+
+    async componentDidMount() {
+        this.setState({ readError: null });
+        try {
+            db.ref("chats").on("value", snapshot => {
+                let chats = [];
+                snapshot.forEach((snap) => {
+                    chats.push(snap.val());
+                });
+                this.setState({ chats });
+            });
+        } catch (error) {
+            this.setState({ readError: error.message });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="chats">
+                    {this.state.chats.map(chat => {
+                        return <p key={chat.timestamp}>{chat.content}</p>
+                    })}
+                </div>
+                {# message form #}
+                <form onSubmit={this.handleSubmit}>
+                    <input onChange={this.handleChange} value={this.state.content}></input>
+                    {this.state.error ? <p>{this.state.writeError}</p> : null}
+                    <button type="submit">Send</button>
+                </form>
+                <div>
+                    Login as: <strong>{this.state.user.email}</strong>
+                </div>
+            </div>
+        );
+    }
+}
